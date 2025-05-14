1800c8152eee4d618a9e095f0584ba68d6ff789a
ShinoYumi <shinoyumi456@gmail.com>
2025-05-14 21:56:49 +0200

    Added czech support Updated files: 	new file:   src/translations/cs.ts

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -1,8 +1,9 @@
 import type { Translation } from "./definition.ts";
 import en from "./en";
+import cs from "./cs";
 
-export const LANGS = ["en"] as const;
+export const LANGS = ["en","cs"] as const;
 
 export type Lang = (typeof LANGS)[number];
 
-export const translations: { [l in Lang]: Translation } = { en };
+export const translations: { [l in Lang]: Translation } = { en, cs };
