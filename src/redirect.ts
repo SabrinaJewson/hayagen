aa6064f4aa9da3d9e6f53dda42e4ac97c89ab3b4
SabrinaJewson <sejewson@gmail.com>
2025-05-17 18:27:33 +0100

    Fix bug

--- src/redirect.ts
+++ src/redirect.ts
@@ -3,8 +3,5 @@ import { LANGS } from "./translations";
 const lang = navigator.language.split("-")[0];
 
 const langs_array: readonly string[] = LANGS;
-if (langs_array.includes(lang)) {
-	location.replace(`${import.meta.env.BASE_URL}/${lang}`);
-} else {
-	location.replace("/en");
-}
+const chosen_lang = langs_array.includes(lang) ? lang : "en";
+location.replace(`${import.meta.env.BASE_URL}/${chosen_lang}`);
