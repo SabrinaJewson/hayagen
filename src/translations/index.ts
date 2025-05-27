abb23b6bed5268b38778dae58294d5ba417c3e68
Weber Jo <yoshi.weber@gmail.com>
2025-05-27 11:58:32 +0200

    Created German translation

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -3,9 +3,10 @@ import type { Translation } from "./definition.ts";
 import en from "./en";
 import fr from "./fr";
 import sk from "./sk";
+import de from "./de";
 
-export const LANGS = ["en", "cs", "sk", "fr"] as const;
+export const LANGS = ["en", "cs", "sk", "fr", "de"] as const;
 
 export type Lang = (typeof LANGS)[number];
 
-export const translations: { [l in Lang]: Translation } = { en, cs, sk, fr };
+export const translations: { [l in Lang]: Translation } = { en, cs, sk, fr, de };
